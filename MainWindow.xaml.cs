60b08d2e5191e9fa753a7a2a7463f083af1a3088
Chreld <christian.eld@outlook.com>
2025-03-31 17:00:06 +0200

    Added more cases to the calculator, and changed the lettering of multiply and division. Resized the output display

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data;
 using System.Linq.Expressions;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MemeCalculator
 {
@@ -42,14 +44,31 @@ namespace MemeCalculator
 
         private void CalculateMemeResponse(string equation)
         {
-            switch (equation)
+            string equationTrimmedLowered = equation.Replace(" ", "").Trim().ToLower();
+            switch (equationTrimmedLowered)
             {
+                case "1+1":
+                    DisplayTextBox.Text = "11";
+                    break;
+                case "2+2":
+                    DisplayTextBox.Text = "Oh we are taking it safe today, aren't we?";
+                    break;
+                case "6+7":
+                    DisplayTextBox.Text = "If 7+7 is 14, then one lower would be 13";
+                    break;
                 case "9+10":
                     DisplayTextBox.Text = "21";
                     break;
                 case "LXIX":
                     DisplayTextBox.Text = "Optime, amice ;)";
                     break;
+                case "7+(-1/2+5)*20":
+                    DisplayTextBox.Text = "Seven-and-a-half-fives. Or 97 if you don't have a potato in your mouth";
+                    break;
+                case "fuckyou":
+                    DisplayTextBox.Text = "Look at the maidenless, who got beef with a calculator. Touch grass";
+                    break;
+                case "Whyis6afraidof7"
                 default:
                     CalculateRealResponse(equation);
                     break;
@@ -61,11 +80,23 @@ namespace MemeCalculator
             var result = new DataTable().Compute(equation, null);
             switch (result)
             {
+                case 13:
+                    DisplayTextBox.Text = "Unlucky 13";
+                    break;
                 case 69:
                     DisplayTextBox.Text = "Nice";
                     break;
+                case 420:
+                    DisplayTextBox.Text = "Grow up";
+                    break;
+                case 666:
+                    DisplayTextBox.Text = "Hello demons, it's me, ya boy";
+                    break;
+                case 911:
+                    DisplayTextBox.Text = "911, what's your emergency?";
+                    break;
                 default:
-                    DisplayTextBox.Text = "ey yo hold up that didn't make any sense";
+                    DisplayTextBox.Text = "ey yo hold up that didn't make any sense. Try again.";
                     break;
             }
         }
